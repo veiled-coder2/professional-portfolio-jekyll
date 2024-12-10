ac67d6d05936a27402ae4b467b6324af589f0191
veiled-coder <abdulfattahrahmat@gmail.com>
2024-12-10 13:42:47 +0100

    added dark/light mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+// THEME
+const themeBtn = document.getElementById("theme-id");
+const body = document.body;
+themeBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-theme");
+  body.classList.contains("dark-theme")
+    ? (themeBtn.textContent = "light mode")
+    : (themeBtn.textContent = "dark mode");
+});
+
+// NAV
 const nav = document.getElementById("main-nav");
 const hamburger = document.getElementById("main-hamburger");
 
