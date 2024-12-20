965a66d85a07d4ec2e708a702be3dd5ae5792c99
veiled-coder <abdulfattahrahmat@gmail.com>
2024-12-20 17:36:18 +0100

    added dynamic width

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,42 @@ hamburger.addEventListener("click", () => {
   nav.classList.toggle("visible-nav");
   hamburger.classList.toggle("transform-hamburger");
 });
+
+//PROJECT FILTER
+console.log("ok");
+document.addEventListener("DOMContentLoaded", function () {
+  const filterBtns = document.querySelectorAll("[data-filter]");
+  const projects = document.querySelectorAll(".project-item");
+
+  filterBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      filterBtns.forEach((button) => {
+        button.style.backgroundColor = "";
+      });
+
+      btn.style.backgroundColor = "green";
+      let novisibleProject = 0;
+      const filtertype = btn.getAttribute("data-filter");
+      projects.forEach((project) => {
+        const stack = project.getAttribute("data-stack");
+        if (filtertype === "all" || stack.includes(filtertype)) {
+          // project.style.width = "100%";
+          project.style.display = "block";
+          novisibleProject++;
+        } else {
+          project.style.display = "none";
+        }
+      });
+
+      if (novisibleProject === 1 && window.innerWidth >= 725) {
+        projects.forEach((project) => {
+          project.style.width = "30%";
+        });
+      } else {
+        projects.forEach((project) => {
+          project.style.width = "100%";
+        });
+      }
+    });
+  });
+});
